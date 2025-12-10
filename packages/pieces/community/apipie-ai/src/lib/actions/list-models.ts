995f45fbab1370514b270b62dfb8074e6f37a5dd
Samsquamptch <m-greening@hotmail.co.uk>
2025-12-10 22:39:17 +0000

    feat: added type and subtype props to list models action

--- packages/pieces/community/apipie-ai/src/lib/actions/list-models.ts
+++ packages/pieces/community/apipie-ai/src/lib/actions/list-models.ts
@@ -1,4 +1,4 @@
-import { createAction } from '@activepieces/pieces-framework';
+import { Property, createAction } from '@activepieces/pieces-framework';
 import { httpClient, HttpMethod } from '@activepieces/pieces-common';
 import { apipieAuth } from '../..';
 
@@ -8,11 +8,57 @@ export const listModels = createAction({
   auth: apipieAuth,
   displayName: 'list models',
   description: 'list models based on selected type',
-  props: {},
+  props: {
+    modelType: Property.StaticDropdown({
+    displayName: 'Type',
+    description: 'Filter by type of model',
+    required: false,
+    options: {
+      options: [
+        { label: 'llm', value: 'llm' },
+        { label: 'vision', value: 'vision' },
+        { label: 'embedding', value: 'embedding' },
+        { label: 'image', value: 'image' },
+        { label: 'voice', value: 'voice' },
+        { label: 'moderation', value: 'moderation' },
+        { label: 'coding', value: 'coding' },
+        { label: 'free', value: 'free' },
+      ],
+  },
+}),
+  modelSubtype: Property.StaticDropdown({
+    displayName: 'Subtype',
+    description: 'Filter by subtype of model',
+    required: false,
+    options: {
+      options: [
+        { label: 'chat', value: 'chat' },
+        { label: 'fill-mask', value: 'fill-mask' },
+        { label: 'question-answering', value: 'question-answering' },
+        { label: 'tts', value: 'tts' },
+        { label: 'stt', value: 'stt' },
+        { label: 'multimodal', value: 'multimodal' },
+      ],
+  },
+}),
+  },
   async run(context) {
+    const modelType = context.propsValue.modelType
+    const modelSubtype = context.propsValue.modelSubtype
+    let getUrl = 'https://apipie.ai/v1/models'
+    if (modelType && modelSubtype) {
+      getUrl = getUrl + '?type=' + modelType + '&subtype=' + modelSubtype
+    } else {
+      if (modelType) {
+        getUrl = getUrl + '?type=' + modelType 
+      }
+      if (modelSubtype) {
+        getUrl + '?subtype=' + modelSubtype 
+      }
+    }
     const res = await httpClient.sendRequest<string[]>({
       method: HttpMethod.GET,
-      url: 'https://apipie.ai/v1/models?type=llm',
+      url: getUrl,
       headers: {
         Authorization: context.auth,
         Accept: "application/json"
